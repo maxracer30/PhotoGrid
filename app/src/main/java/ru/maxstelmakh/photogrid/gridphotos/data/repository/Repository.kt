3c577f7dc220388001564a73b7f9054f011d0dd3
maxstelmakh <maxracer30@gmail.com>
2023-05-10 20:59:48 +0300

    Delete old repository

--- app/src/main/java/ru/maxstelmakh/photogrid/gridphotos/data/repository/Repository.kt
+++
@@ -1,9 +0,0 @@
-package ru.maxstelmakh.photogrid.gridphotos.data.repository
-
-import kotlinx.coroutines.flow.Flow
-import ru.maxstelmakh.photogrid.gridphotos.data.model.SearchedPhotos
-import ru.maxstelmakh.photogrid.utils.Result
-
-interface Repository {
-    suspend fun fetch(): Flow<Result<SearchedPhotos>>
-}
